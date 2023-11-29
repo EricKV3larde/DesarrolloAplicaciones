eff779f1833a2b5856f4de347ba6e056abb94ee2
Erick Velarde <erick.velarde@ucsm.edu.pe>
2023-11-29 11:38:39 -0500

    14

--- DesP_Practica_08/pruebaflask/exp6/ejecucion.py
+++
@@ -1,8 +0,0 @@
-from estudiante import Estudiante
-
-estudiante = Estudiante()
-estudiante.ingresarDatos()
-estudiante.imprimirDatos()
-estudiante.matricular()
-estudiante.pagarPension()
-estudiante.imprimirDatos()
